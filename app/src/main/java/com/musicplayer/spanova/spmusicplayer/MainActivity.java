0fa80e5795527e9a5d117f1706fd71fb0c8956c6
Sofia Panova <sofiqpanova@gmail.com>
2018-04-27 22:40:28 +0300

    list mp3 files

--- app/src/main/java/com/musicplayer/spanova/spmusicplayer/MainActivity.java
+++ app/src/main/java/com/musicplayer/spanova/spmusicplayer/MainActivity.java
@@ -1,13 +1,203 @@
 package com.musicplayer.spanova.spmusicplayer;
-
+import android.Manifest;
+import android.provider.MediaStore;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
+import android.os.Build;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.database.Cursor;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context;
+
+    public static final int RUNTIME_PERMISSION_CODE = 7;
+
+    String[] ListElements = new String[] { };
+
+    ListView listView;
+
+    List<String> ListElementsArrayList ;
+
+    ArrayAdapter<String> adapter ;
+
+    ContentResolver contentResolver;
+
+    Cursor cursor;
+
+    Uri uri;
+
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView1);
+
+        button = (Button)findViewById(R.id.button);
+
+        context = getApplicationContext();
+
+        ListElementsArrayList = new ArrayList<>(Arrays.asList(ListElements));
+
+        adapter = new ArrayAdapter<String>
+                (MainActivity.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
+
+        // Requesting run time permission for Read External Storage.
+        AndroidRuntimePermission();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                GetAllMediaMp3Files();
+
+                listView.setAdapter(adapter);
+
+            }
+        });
+
+        // ListView on item selected listener.
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // TODO Auto-generated method stub
+                // Showing ListView Item Click Value using Toast.
+
+                Toast.makeText(MainActivity.this,parent.getAdapter().getItem(position).toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+
+    public void GetAllMediaMp3Files(){
+
+        contentResolver = context.getContentResolver();
+
+        uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
+        cursor = contentResolver.query(
+                uri, // Uri
+                null,
+                null,
+                null,
+                null
+        );
+
+        if (cursor == null) {
+
+            Toast.makeText(MainActivity.this,"Something Went Wrong.", Toast.LENGTH_LONG);
+
+        } else if (!cursor.moveToFirst()) {
+
+            Toast.makeText(MainActivity.this,"No Music Found on SD Card.", Toast.LENGTH_LONG);
+
+        }
+        else {
+
+            int Title = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+
+            //Getting Song ID From Cursor.
+            //int id = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+
+            do {
+
+                // You can also get the Song ID using cursor.getLong(id).
+                //long SongID = cursor.getLong(id);
+
+                String SongTitle = cursor.getString(Title);
+
+                // Adding Media File Names to ListElementsArrayList.
+                ListElementsArrayList.add(SongTitle);
+
+            } while (cursor.moveToNext());
+        }
+    }
+
+    // Creating Runtime permission function.
+    public void AndroidRuntimePermission(){
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                    AlertDialog.Builder alert_builder = new AlertDialog.Builder(MainActivity.this);
+                    alert_builder.setMessage("External Storage Permission is Required.");
+                    alert_builder.setTitle("Please Grant Permission.");
+                    alert_builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            ActivityCompat.requestPermissions(
+                                    MainActivity.this,
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    RUNTIME_PERMISSION_CODE
+
+                            );
+                        }
+                    });
+
+                    alert_builder.setNeutralButton("Cancel",null);
+
+                    AlertDialog dialog = alert_builder.create();
+
+                    dialog.show();
+
+                }
+                else {
+
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            RUNTIME_PERMISSION_CODE
+                    );
+                }
+            }else {
+
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+
+        switch(requestCode){
+
+            case RUNTIME_PERMISSION_CODE:{
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                }
+                else {
+
+                }
+            }
+        }
     }
 }
